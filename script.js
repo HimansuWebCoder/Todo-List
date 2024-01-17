7a4afc0a213f13c1c0342a685b83d2cd229e72e0
Hima-123797 <113868318+Hima-123797@users.noreply.github.com>
2024-01-17 13:34:47 +0530

    updated new todo

--- script.js
+++ script.js
@@ -1,61 +1,33 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const output = document.querySelector("#output");
 
-
-// DRY Don't Repeat Yourself
-// Let's refactoring 
-
-function inputLength() {
-  return input.value.length;
-}
-
-function createListElement() {
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(input.value));
-      ul.appendChild(li);
-      input.value = "";
-}
-
-function addListAfterClick() {
-  if (inputLength() > 0) {
-     createListElement();
+// create a function where input todo value add
+function handleBtnValue() {
+  if (input.value !== "") {  // if input value is empty then create any input value list otherwise not
+    const list = document.createElement("li");  // create a list (li) where all input value stored
+    output.appendChild(list); // append the list variable to it's parent ul (un-ordered list)
+    list.textContent = input.value; // store the input value when something creating todo list value and showing in list.
+	list.classList.add("fontSize");
+    input.value = ""; // after adding the value and will show then input value going to be empty
+    input.focus(); // and then automatically going to input value focus
   }
 }
 
-function addListAfterKeypress(event) {
-     if (inputLength() > 0 && event.keyCode === 13) {
-         createListElement();
-  }
-}
-
-function focusedInput() {
+// create a function where the value of input create a todo when click the button 
+function handleInputValue(e) {
+  if (input.value !== "" && e.key === "Enter") { // when value is not empty then it will not add any value otherwise do that and after value add and enter within input it show in the list.
+    const list = document.createElement("li");
+    output.appendChild(list);
+    list.textContent = input.value;
+	list.classList.add("fontSize");
+    input.value = "";
     input.focus();
+  }
 }
 
-button.addEventListener("click",  addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
-button.addEventListener("click", focusedInput);
 
 
-
-// Above we have written code clean and with refactoring-
-// button.addEventListener("click", function() {
-//   if (input.value.length > 0) {
-//       var li = document.createElement("li");
-//       li.appendChild(document.createTextNode(input.value));
-//       ul.appendChild(li);
-//       input.value = "";
-//   }
-// })
-
-// input.addEventListener("keypress", function(event) {
-//      if (input.value.length > 0 && event.keyCode === 13) {
-//       var li = document.createElement("li");
-//       li.appendChild(document.createTextNode(input.value));
-//       ul.appendChild(li);
-//       input.value = "";
-//   }
-// })
+// finally call the event using input and btn element.
+btn.addEventListener("click", handleBtnValue);
+input.addEventListener("keydown", handleInputValue);
